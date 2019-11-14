2cc06d6918d9ec34d83bc98e41618bd07c1ae2ff
your usernametatadavila <tata.davila.a@gmail.com>
2019-11-13 19:14:56 -0500

    Built for heroku

--- src/components/develop/VideoQuiz.vue
+++ src/components/develop/VideoQuiz.vue
@@ -20,6 +20,6 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="css">
 .videoQuiz {
-  background-color: blue;
+  /* background-color: blue; */
 }
 </style>
